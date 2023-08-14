1b6b38f3f5b0d2d8f1dd009e79712280a508a3e5
Michael Yong <wrong92@gmail.com>
2023-08-14 14:06:38 -0700

    wrap

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 /**
- * 車でトけま第属リオヘシ都門ぜき探扱ミヒカ耐駿らいぼリ使代川そトゃ賞庫ネ文散らあ生20母ン見支互札永あつすド。道チカエ嫌打ヘソメシ広元マヲカ康入レ活舵カソ況市7中レム給昭真記ドめざ者月モ月善カヘ割転こ東2景ミエ投二イモル済葉クぴス際勝ら処像ル販保嗅尽を。倍更イユタヱ下同江い物高ひ表図ソネ県者る写無関クすこス憤理スくけか詳待じルが共59志乞9地闘サアニヒ一込京一こち。
+ * 車でトけま第属リオヘシ都門ぜき探扱ミヒカ耐駿らいぼリ使代川そトゃ賞庫ネ文散らあ生20母ン見支互札永あつすド。
+ * 道チカエ嫌打ヘソメシ広元マヲカ康入レ活舵カソ況市7中レム給昭真記ドめざ者月モ月善カヘ割転こ東2景ミエ投二イモル済葉クぴス際勝ら処像ル販保嗅尽を。
+ * 倍更イユタヱ下同江い物高ひ表図ソネ県者る写無関クすこス憤理スくけか詳待じルが共59志乞9地闘サアニヒ一込京一こち。
  */
 function foo() {}
 
 /**
- * Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+ * Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ * Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+  * when an unknown printer took a galley of type and scrambled it to make a
+  * type specimen book. It has survived not only five centuries, but also the
+  * leap into electronic typesetting, remaining essentially unchanged. It was
+  * popularised in the 1960s with the release of Letraset sheets containing
+  * Lorem Ipsum passages, and more recently with desktop publishing software
+  * like Aldus PageMaker including versions of Lorem Ipsum.
  */
 function bar() {}
